69a14fd646c048c4165b1d90f9cc27345435f167
vijay samrath <vijaysamrath462@gmail.com>
2023-01-14 20:59:11 +0530

    Modified test case

--- test/MyToken.js
+++ test/MyToken.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Nft Auction", async function () {
+describe("Fractionalise Nft", async function () {
 
     async function deploy() {
         
@@ -39,25 +39,60 @@ describe("Nft Auction", async function () {
     })
 
     it("minting Token & giving allowance", async () => {
-      await nft.safeMint(accounts[1].address, 1)
-      console.log("Balance of account", await nft.balanceOf(accounts[1].address));
 
-      await nft.connect(accounts[1]).setApprovalForAll(myToken.address, true)
+      await nft.safeMint(accounts[0].address, 1)
+      console.log("Nft balance of account[0]", await nft.balanceOf(accounts[0].address));
+
+      await nft.connect(accounts[0]).setApprovalForAll(myToken.address, true)
         
     })
 
     it("initializing", async () => {
 
-        await myToken.connect(accounts[0]).connect(accounts[1]).initialize(nft.address, 1, 100000000000)
-
-        console.log("Owner of account", await nft.ownerOf(1));
+      await myToken.connect(accounts[0]).initialize(nft.address, 1, 100000000000)
+      console.log("Owner of Nft", await nft.ownerOf(1))
+      console.log("Token balance of account[0]", await myToken.balanceOf(accounts[0].address))
     
+    })
+
+    it("transfer Token to the account 1", async() => {
+      await myToken.connect(accounts[0]).transfer(accounts[1].address, 100000)
+      console.log("Token balance of account[1]", await myToken.balanceOf(accounts[1].address))
+      console.log("Token balance of account[0]", await myToken.balanceOf(accounts[0].address))
+
+    })
+
+    it("put for sale ", async () =>{
+      await myToken.connect(accounts[0]).putForSale(ethers.utils.parseEther("10"))
+      // using ethers.utils.parseUnits("10000000000000000000000000") for converting Big no. in strings 
+      console.log("sale started", await myToken.forSale())
+      console.log("saleing price", await myToken.salePrice())
+
+    })
+
+    it("purchase", async() => {
+
+      await myToken.connect(accounts[5]).purchase({value:ethers.utils.parseEther("10")})
+      console.log("Nft purchased by", await nft.ownerOf(1))
+      console.log("ethers available in account 5:", await ethers.provider.getBalance(accounts[5].address))
+
+    })
+
+    it("redeem", async() => {
+
+      await myToken.connect(accounts[0]).redeem(99999900000)
+      console.log("balance of account 0", await ethers.provider.getBalance(accounts[0].address))
+      console.log("Token balance of account[0]", await myToken.balanceOf(accounts[0].address))
+
+
       })
 
+    it("redeem", async() => {
 
-
-
-
-
+      await myToken.connect(accounts[1]).redeem(100000)
+       
+      console.log("balance of account 1", await ethers.provider.getBalance(accounts[1].address))
+      console.log("Token balance of account[1]", await myToken.balanceOf(accounts[1].address))
+    })
 
 })
